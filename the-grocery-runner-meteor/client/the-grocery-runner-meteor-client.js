1bf601f4261f3a338411e8bb400a30033ea292e7
Darryl Murray <darrylmurray@me.com>
2015-11-14 15:18:49 -0500

    setup for meteor.

---
+++ the-grocery-runner-meteor/client/the-grocery-runner-meteor-client.js
@@ -0,0 +1,18 @@
+/**
+ * Created by darryl on 2015-11-14.
+ */
+// counter starts at 0
+Session.setDefault('counter', 0);
+
+Template.hello.helpers({
+    counter: function () {
+        return Session.get('counter');
+    }
+});
+
+Template.hello.events({
+    'click button': function () {
+        // increment the counter when button is clicked
+        Session.set('counter', Session.get('counter') + 1);
+    }
+});
---
+++ the-grocery-runner-meteor/server/the-grocery-runner-meteor-server.js
@@ -0,0 +1,6 @@
+/**
+ * Created by darryl on 2015-11-14.
+ */
+Meteor.startup(function () {
+    // code to run on server at startup
+});
