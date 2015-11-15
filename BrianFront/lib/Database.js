e7e493cf5a74aa1cc0c95719458a845a9c57daa9
Darryl Murray <darrylmurray@me.com>
2015-11-14 21:30:25 -0500

    Set up object prototypes for database objects.

--- BrianFront/lib/Database.js
+++ BrianFront/lib/Database.js
@@ -1,1 +1,27 @@
 GroceryList = new Mongo.Collection("grocerylist");
+
+var ItemDesignation = {
+    OWNER : 1,
+    HOUSEHOLD : 2
+}
+
+// Note, this defaults to owner if invalid
+function ListItems (name, quantity, owner, designation) {
+    this.name = name;
+    this.quantity = quantity;
+    this.owner = owner;
+
+    if (designation === ItemDesignation.OWNER || designation === ItemDesignation.HOUSEHOLD) {
+        this.designation = designation;
+    }
+    else {
+        this.designation = ItemDesignation.OWNER;
+    }
+
+    this.price = 0;
+}
+
+function GroceryListObject(listName) {
+    this.name = listName;
+    this.Items = [];
+}
